263a788b61978e68cefbf82834e9ef2884c15e2f
Sergey Budaev <sergeybudaev999@gmail.com>
2025-10-26 15:58:38 +0200

    Refactor Core.db

--- app/core/init_db.py
+++ app/core/init_db.py
@@ -1,12 +1,21 @@
-from app.core.db_users import engine_users, UserBase
 from sqlalchemy import create_engine
+from sqlalchemy.ext.asyncio import create_async_engine
+from app.core.db_users import engine_users, UserBase
+from app.core.db_history import engine_history, HistoryBase
+from app.core.db_tenant import TenantBase
 from app.core.settings import settings
-from app.core.db_history import HistoryBase
 
-async def init_db():
+# users.db
+async def create_user_database():
     async with engine_users.begin() as conn:
         await conn.run_sync(UserBase.metadata.create_all)
 
-def create_history_database():
-    engine = create_engine(settings.HISTORY_DB_URL.replace("sqlite+aiosqlite", "sqlite"))
-    HistoryBase.metadata.create_all(engine)
+# history.db
+async def create_history_database():
+    async with engine_history.begin() as conn:
+        await conn.run_sync(HistoryBase.metadata.create_all)
+
+# tenant-базы
+def create_tenant_database(db_path: str):
+    engine = create_engine(f"sqlite:///{db_path}")
+    TenantBase.metadata.create_all(engine)
